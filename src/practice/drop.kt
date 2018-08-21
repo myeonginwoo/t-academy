355a605abf08a7234880e221a108da43b253aa80
lazysou <myeonginwoo@gmail.com>
2018-08-21 22:30:31 +0900

    added fpList example

---
+++ src/practice/drop.kt
@@ -0,0 +1,12 @@
+package practice
+
+fun main(args: Array<String>) {
+    val intlist = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil)))))
+
+    require(intlist.drop(0) == Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil))))))
+    require(intlist.drop(3) == Cons(4, Cons(5, Nil)))
+    require(intlist.drop(6) == Nil)
+
+}
+
+tailrec fun <T> FpList<T>.drop(n: Int): FpList<T> = TODO()
